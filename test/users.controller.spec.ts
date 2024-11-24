5815fabd9934e943a70c155e7c1709d92893f6af
raphaelkauan <raphaelkauanoficial@gmail.com>
2024-11-24 16:49:30 -0400

    feat(database): adicionando nova coluna na tabela historico

--- test/users.controller.spec.ts
+++ test/users.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from '../src/modules/users/users.controller';
+import { UsersController } from '../src/modules/user/users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
